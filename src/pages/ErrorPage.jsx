ad79fb636d1bbc7ecf07027771381b8bf632fcb9
Nabin Islam <sinabin62246@gmail.com>
2024-04-04 12:27:17 +0600

    added error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <main class="flex flex-col h-screen justify-center items-center bg-gray-100">
+      <div class="flex flex-col items-center">
+        <h1 class="text-[120px] font-extrabold text-gray-700">404</h1>
+        <p class="text-2xl font-medium text-gray-600 mb-6">Page Not Found</p>
+        <Link
+          to="/"
+          class="px-4 py-2 font-medium text-white bg-indigo-500 rounded-md hover:bg-indigo-600 transition-all duration-200 ease-in-out"
+        >
+          Go Home
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default ErrorPage;
